1d15fa736bd453fe4f1ad6e9b510b5ee09f58dfd
Alexey <87129973+AlexFoxalt@users.noreply.github.com>
2021-09-10 15:11:34 +0300

    Another version of cacher added.

---
+++ HW1[versatility_version].py
@@ -0,0 +1,41 @@
+from functools import wraps
+
+
+class CachedItem:
+    def __init__(self, key: tuple, data: str, counter: int):
+        self.key = key
+        self.data = data
+        self.counter = counter
+
+    def __str__(self):
+        return f'KEY ------>  {self.key}' \
+               f'DATA ----->  {self.data}\n' \
+               f'COUNTER -->  {self.counter}'
+
+    def __eq__(self, other):
+        return self.key == other
+
+
+def response_from_db(obj: CachedItem):
+    obj.counter += 1
+    return f'[from cache] {obj.data}'
+
+
+def cache(limit=10):
+    def shaper(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            keys = (args, tuple(kwargs.items()))
+            for obj in CachedData:
+                if keys == obj:
+                    return response_from_db(obj)
+            data = function(keys)
+            if len(CachedData) >= limit:
+                CachedData.sort(key=lambda i: i.counter, reverse=True)
+                CachedData.pop()
+            CachedData.append(CachedItem(keys, data, 1))
+            return data
+        return wrapper
+    CachedData = []  # [CachedItem(key,data,counter), CachedItem(key,data,counter)]
+    return shaper
+
