ba14d0128ebc188e45347e083f55038ad026af73
Alexey <87129973+AlexFoxalt@users.noreply.github.com>
2021-09-15 13:54:39 +0300

    Added another version on password generator

---
+++ HW3/HW3[2nd version of pass generator].py
@@ -0,0 +1,72 @@
+import random
+import string
+from flask import request, Flask
+
+UPPER_LETTERS = string.ascii_uppercase
+LOWER_LETTERS = string.ascii_lowercase
+DIGITS = string.digits
+SPECIALS = string.punctuation
+
+
+def parse_params(arg: dict):
+    """
+    Fetch params from URL. Check all of possible errors.
+
+    :param arg: Expected dict of params parsed from URL string.
+    :return: Values of 3 options for password generator machine.
+    """
+    try:
+        length = int(arg.get('length', 10)) if int(arg.get('length', 10)) in range(1, 101) else 10
+    except ValueError:
+        length = 10
+
+    try:
+        digits = int(arg.get('digits', 0))
+    except ValueError:
+        digits = 0
+
+    try:
+        specials = int(arg.get('specials', 0))
+    except ValueError:
+        specials = 0
+
+    return length, digits, specials
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_password():
+    """
+    Take values from parse_params() function, then pass them to the password generator.
+
+    :return: Random password as .html file.
+    """
+    params = request.args
+
+    length, digits, specials = parse_params(params)
+
+    string_generator = [*UPPER_LETTERS, *LOWER_LETTERS]
+
+    if digits:
+        string_generator.extend(DIGITS)
+        digits = '✔ Digits included'
+    else:
+        digits = '✘ Digits NOT included'
+
+    if specials:
+        string_generator.extend(SPECIALS)
+        specials = '✔ Specials included'
+    else:
+        specials = '✘ Specials not included'
+
+    password = ''.join(random.choices(string_generator, k=length))
+
+    length = 'Length = ' + str(length)
+
+    return '<br>'.join((length, digits, specials, password))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
