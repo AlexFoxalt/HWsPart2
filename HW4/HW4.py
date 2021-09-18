bec7a69357433ae5f83a0f587561790eb9118419
Alexey <87129973+AlexFoxalt@users.noreply.github.com>
2021-09-18 11:50:49 +0300

    Empty file

---
+++ HW4/HW4.py
@@ -0,0 +1,10 @@
+"""HW4 Чернышов Алексей"""
+
+
+# Создать PR/MR с любыми изменениями либо с выполненным д/з.
+
+def some_foo(arg: str):
+    print(arg)
+
+
+some_foo("Content for merging into master")
