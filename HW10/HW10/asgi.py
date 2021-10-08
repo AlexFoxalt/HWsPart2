2c54c757ebbd5966698312f7ec5048fcd12d6b47
Alexey <87129973+AlexFoxalt@users.noreply.github.com>
2021-10-08 13:55:28 +0300

    Whole project reworked in OOP style

--- HW10/HW10/asgi.py
+++ HW10/HW10/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HW10 project.
+ASGI config for HW[10,11] project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HW10.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HW[10,11].settings')
 
 application = get_asgi_application()
