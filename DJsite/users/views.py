ff72bb9404a1d0f6bc81ac6f628db98a680902ea
Alexey <87129973+AlexFoxalt@users.noreply.github.com>
2021-10-10 10:09:48 +0300

    No changes

--- DJsite/users/views.py
+++ DJsite/users/views.py
@@ -10,6 +10,7 @@ from .utils import teacher_filter_query, student_filter_query, get_int_count, ho
     EntitySearchPerOneFieldMixin, EntitySearchPerAllFieldsMixin
 from .models import Student, Teacher
 
+# Create your views here.
 parser = djangoparser.DjangoParser()
 
 
