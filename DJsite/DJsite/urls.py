98c1a215e21eaecbf8cf1eb20cdd0140734c32c8
Alexey <87129973+AlexFoxalt@users.noreply.github.com>
2021-10-18 17:32:39 +0300

    Minor changes

--- DJsite/DJsite/urls.py
+++ DJsite/DJsite/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
 
+from users.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,3 +35,5 @@ if settings.DEBUG:
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = page_not_found
